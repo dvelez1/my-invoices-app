67e70b29eb032fc26054cba57676f4a797851147
Dennis Velez <36821370+dvelez1@users.noreply.github.com>
2022-07-14 12:47:02 -0400

    Add Input element Customer Upster

--- src/components/Customers/CustomerUpsert.tsx
+++ src/components/Customers/CustomerUpsert.tsx
@@ -1,20 +1,127 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const CustomerUpsert = () => {
+  const navigate = useNavigate();
+  const handleUpsertClick = () => {
+    navigate("/customer");
+  };
+
   return (
     <>
       <div className="card">
         <h3 className="card-header">Add/Edit Customer</h3>
         <div className="card-body">
-          <h5 className="card-title">Welcome to the Invoice Application</h5>
-          <p className="card-text">
-            TThe purpose of this application, is provide a simple solution to
-            manage the day to day of small offices. .
-          </p>
+
+
+          <div className="row">
+            <div className="col-md-3">
+              <label className="form-label">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                name="name"
+                placeholder="Name"
+              />
+            </div>
+            <div className="col-md-3">
+              <label className="form-label">Middle Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="middleName"
+                name="middleName"
+                placeholder="Middle Name"
+              />
+            </div>
+            <div className="col-md-3">
+              <label className="form-label">First Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="firstName"
+                name="firstName"
+                placeholder="First Name"
+              />
+            </div>
+            <div className="col-md-3">
+              <label className="form-label">Last Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="lastName"
+                name="lastName"
+                placeholder="Last Name"
+              />
+            </div>
+          </div>
+
+          <div className="row mt-2">
+            <div className="col-md-6">
+              <label className="form-label">Adress 1</label>
+              <input
+                type="text"
+                className="form-control"
+                id="address1"
+                name="address1"
+                placeholder="Address 1"
+              />
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Adress 2</label>
+              <input
+                type="text"
+                className="form-control"
+                id="address2"
+                name="address2"
+                placeholder="Address 2"
+              />
+            </div>
+          </div>
+
+          <div className="row mt-2 mb-2">
+            <div className="col-md-4">
+              <label className="form-label">City</label>
+              <input
+                type="text"
+                className="form-control"
+                id="city"
+                name="city"
+                placeholder="City"
+              />
+            </div>
+            <div className="col-md-4">
+              <label className="form-label">State</label>
+              <input
+                type="text"
+                className="form-control"
+                id="state"
+                name="state"
+                placeholder="State"
+              />
+            </div>
+            <div className="col-md-4">
+              <label className="form-label">Zip Code</label>
+              <input
+                type="text"
+                className="form-control"
+                id="zip-code"
+                name="zip-code"
+                placeholder="Zip Code"
+              />
+            </div>
+          </div>
+
+          <button
+            type="button"
+            className="btn btn-primary mt-2"
+            onClick={handleUpsertClick}
+          >
+            Cancel
+          </button>
         </div>
       </div>
     </>
-  )
-}
-
-
+  );
+};
