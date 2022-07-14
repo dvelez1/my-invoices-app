56496682c5348558dca5957ab874fcb77aa87c73
Dennis Velez <36821370+dvelez1@users.noreply.github.com>
2022-07-13 23:31:22 -0400

    Navbar Link Implementation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
+        <Link className="navbar-brand" to="/">
           My Invoices
-        </a>
+        </Link>
+
         <button
           className="navbar-toggler"
           type="button"
@@ -20,20 +22,20 @@ export const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarText">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <Link className="nav-link" to="/customer">
                 Customers
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <Link className="nav-link" to="/product">
                 Products
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
