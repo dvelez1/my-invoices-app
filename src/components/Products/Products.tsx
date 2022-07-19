725db4d2ae06329196bbbe4d9d04ad29ef06fadc
Dennis Velez <36821370+dvelez1@users.noreply.github.com>
2022-07-19 15:08:34 -0400

    Initial Product Layout

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,11 +1,150 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Product } from "../../models/product";
+
+// Data Context
+
+// Import Spinner
+import { Loading } from "../../components/shared/Loading";
 
 export const Products = () => {
+  // Add DataContext
+
+  // Add Get
+  const [currentPage, setCurrentPage] = useState(0);
+  const [search, setSearch] = useState("");
+
+  //#region "Methods"
+
+  const navigate = useNavigate();
+  const handleUpsertClick = () => {
+    navigate("/productUpsert");
+  };
+
+  // Redirect to Main Page
+  function handleEditClick(customerId: Number) {
+    // setCustomerModel(
+    //   customers.filter((obj) => {
+    //     return obj.CustomerId === customerId;
+    //   })[0]
+    // );
+    // if (customerId > 0) handleUpsertClick();
+  }
+
+  //#region "Filtering and Pagination"
+  // const filteredProduct = (): Product[] => {
+  //   if (search.length === 0)
+  //     return products.slice(currentPage, currentPage + 10);
+
+  //   //Search Input with data
+  //   const filtered = products.filter((prod) => prod.Name.toLowerCase().includes(search));
+  //   return filtered.slice(currentPage, currentPage + 10);
+  // };
+
+  const nextPage = () => {
+    // if (
+    //   products.filter((prod) => prod.Name.includes(search)).length >
+    //   currentPage + 10
+    // )
+    //   setCurrentPage(currentPage + 10);
+  };
+
+  const prevPage = () => {
+     if (currentPage > 0) setCurrentPage(currentPage - 10);
+  };
+
+  const onSearchChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentPage(0);
+    setSearch(target.value.toLowerCase());
+  };
+
+  //#endregion "Filtering and Pagination"
+
+  //#endregion "Methods"
+
   return (
-    <div>
-      <p>Products</p>
-    </div>
-  )
-}
+    <>
+      <div className="card">
+        <h3 className="card-header">Products</h3>
+        <div className="card-body">
+          <h5 className="card-title">List of Products</h5>
+          <hr />
 
+          <input
+            className="mb-2 form-control"
+            type="text"
+            placeholder="Products Search by Name"
+            value={search}
+            onChange={onSearchChange}
+          />
 
+          <button
+            type="button"
+            className="btn btn-primary mt-2"
+            onClick={handleUpsertClick}
+          >
+            Create New Product
+          </button>
+          <table className="table mt-2">
+            <thead className="thead-dark">
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+
+            {/* <tbody>
+              {filteredCustomers().map(
+                ({
+                  CustomerId,
+                  Name,
+                  FirstName,
+                  LastName,
+                  Address1,
+                  Address2,
+                  City,
+                  State,
+                  ZipCode,
+                }) => (
+                  <tr key={CustomerId}>
+                    <td>{CustomerId}</td>
+                    <td>{Name + " " + FirstName + " " + LastName}</td>
+                    <td>{Address1}</td>
+                    <td>{Address2}</td>
+                    <td>{City}</td>
+                    <td>{State}</td>
+                    <td>{ZipCode}</td>
+                    <td>
+                      <span className="me-md-2 ">
+                        <i
+                          title="Edit Customer"
+                          className="bi bi-pencil-square cursor"
+                          style={{ fontSize: 25 }}
+                          onClick={() => handleEditClick(CustomerId)}
+                        ></i>
+                      </span>
+                    </td>
+                  </tr>
+                )
+              )}
+            
+            
+            </tbody> */}
+          </table>
+          <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+            <button className="btn btn-primary me-md-2" onClick={prevPage}>
+              Previous
+            </button>
+            <button className="btn btn-primary" onClick={nextPage}>
+              Next
+            </button>
+          </div>
+          {/* {isLoading && <Loading />} */}
+        </div>
+      </div>
+    </>
+  );
+};
