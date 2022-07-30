e6cbaebbe5f571b167ab073cb92d21ffe1b79c07
Dennis Velez <36821370+dvelez1@users.noreply.github.com>
2022-07-30 00:35:40 -0400

    Add container

--- src/components/Invoice/InvoiceUpsert.tsx
+++ src/components/Invoice/InvoiceUpsert.tsx
@@ -116,8 +116,7 @@ export const InvoiceUpsert = () => {
       Quantity: Number(event.target.quantity.value),
     };
 
-    setInvoiceDetailsArray(current => [...current, formData]);
-    
+    setInvoiceDetailsArray((current) => [...current, formData]);
   };
 
   return (
@@ -282,92 +281,100 @@ export const InvoiceUpsert = () => {
                 <div className="card mt-2" style={{ width: "100" }}>
                   <div className="card-body">
                     <h5 className="card-title">Details</h5>
-
-                    <label className="fw-bold mb-2">
-                      Add detail to the Invoice:
-                    </label>
-                    <form onSubmit={handleAddInvoiceDetailsSubmit}>
-                      <div className="row">
-                        <div className="col-md-3">
-                          <label className="form-label fw-bold">Product</label>
-                          <select
-                            name="productId"
-                            className="form-control"
-                            aria-label="Floating label select example"
-                            onChange={(e) =>
-                              setProductPrice(
-                                products.filter((obj) => {
-                                  return (
-                                    obj.ProductId == Number(e.target.value)
-                                  );
-                                })[0].Price
-                              )
-                            }
-                          >
-                            <option value="Select a Product">
-                              {" "}
-                              -- Select a Product --{" "}
-                            </option>
-                            {products.map((prod) => (
-                              <option
-                                key={prod.ProductId}
-                                value={prod.ProductId}
+                    <div className="card mt-2" style={{ width: "100" }}>
+                      <div className="card-body">
+                        <label className="fw-bold mb-2">
+                          Add detail to the Invoice:
+                        </label>
+                        <form onSubmit={handleAddInvoiceDetailsSubmit}>
+                          <div className="row">
+                            <div className="col-md-3">
+                              <label className="form-label fw-bold">
+                                Product
+                              </label>
+                              <select
+                                name="productId"
+                                className="form-control"
+                                aria-label="Floating label select example"
+                                onChange={(e) =>
+                                  setProductPrice(
+                                    products.filter((obj) => {
+                                      return (
+                                        obj.ProductId == Number(e.target.value)
+                                      );
+                                    })[0].Price
+                                  )
+                                }
                               >
-                                {prod.Name}
-                              </option>
-                            ))}
-                          </select>
-                        </div>
-                        <div className="col-md-2">
-                          <label className="form-label fw-bold">
-                            Catalog Price
-                          </label>
-                          <input
-                            type="number"
-                            step=".01"
-                            className="form-control"
-                            name="catalogPrice"
-                            placeholder="Catalog Price"
-                            value={productPrice ?? 0}
-                            readOnly
-                          />
-                        </div>
-                        <div className="col-md-2">
-                          <label className="form-label fw-bold">Price</label>
-                          <input
-                            type="number"
-                            step=".01"
-                            className="form-control"
-                            name="price"
-                            placeholder="Price"
-                          />
-                        </div>
-                        <div className="col-md-2">
-                          <label className="form-label fw-bold">Quantity</label>
-                          <input
-                            type="number"
-                            className="form-control"
-                            name="quantity"
-                            placeholder="Quantity"
-                          />
-                        </div>
-                        <div className="col-md-3">
-                          <div className="d-grid gap-2 d-md-flex mt-2">
-                            <label className="form-label fw-bold"></label>
-                            <button
-                              className="btn btn-primary btn-md mt-4"
-                              type="submit"
-                            >
-                              Add
-                            </button>
-                            <button className="btn btn-primary btn-md mt-4">
-                              Clear
-                            </button>
+                                <option value="Select a Product">
+                                  {" "}
+                                  -- Select a Product --{" "}
+                                </option>
+                                {products.map((prod) => (
+                                  <option
+                                    key={prod.ProductId}
+                                    value={prod.ProductId}
+                                  >
+                                    {prod.Name}
+                                  </option>
+                                ))}
+                              </select>
+                            </div>
+                            <div className="col-md-2">
+                              <label className="form-label fw-bold">
+                                Catalog Price
+                              </label>
+                              <input
+                                type="number"
+                                step=".01"
+                                className="form-control"
+                                name="catalogPrice"
+                                placeholder="Catalog Price"
+                                value={productPrice ?? 0}
+                                readOnly
+                              />
+                            </div>
+                            <div className="col-md-2">
+                              <label className="form-label fw-bold">
+                                Price
+                              </label>
+                              <input
+                                type="number"
+                                step=".01"
+                                className="form-control"
+                                name="price"
+                                placeholder="Price"
+                              />
+                            </div>
+                            <div className="col-md-2">
+                              <label className="form-label fw-bold">
+                                Quantity
+                              </label>
+                              <input
+                                type="number"
+                                className="form-control"
+                                name="quantity"
+                                placeholder="Quantity"
+                              />
+                            </div>
+                            <div className="col-md-3">
+                              <div className="d-grid gap-2 d-md-flex mt-2">
+                                <label className="form-label fw-bold"></label>
+                                <button
+                                  className="btn btn-primary btn-md mt-4"
+                                  type="submit"
+                                >
+                                  Add
+                                </button>
+                                <button className="btn btn-primary btn-md mt-4">
+                                  Clear
+                                </button>
+                              </div>
+                            </div>
                           </div>
-                        </div>
+                        </form>
                       </div>
-                    </form>
-
+                    </div>
                     {/* Detais */}
                     <hr />
                     <table className="table table-sm mt-2">
