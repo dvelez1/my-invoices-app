25cfe9564e030e4fbabc3c9c332d036a9a0ce7c6
Dennis Velez <36821370+dvelez1@users.noreply.github.com>
2022-08-16 22:31:39 -0400

    Change Label Bold Size

--- src/components/Products/ProductUpsertBody.tsx
+++ src/components/Products/ProductUpsertBody.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 
-export const ProductUpsertBody = (props:any) => {
+export const ProductUpsertBody = (props: any) => {
   return (
     <>
       <div className="row">
         <div className="col-md-3">
-          <label className="form-label">Name</label>
+          <label className="form-label">
+            <strong>Name</strong>{" "}
+          </label>
           <input
             type="text"
             className="form-control"
             name="Name"
             placeholder="Name"
             // defaultValue={props.product?.Name}
-            value = {props.product.Name}
+            value={props.product.Name}
             onChange={props.handleChange}
           />
           <p className="text-danger"> {props.formErrors.Name}</p>
         </div>
         <div className="col-md-3">
-          <label className="form-label">Price</label>
+          <label className="form-label">
+            <strong>Price</strong>
+          </label>
           <input
             type="number"
             className="form-control"
@@ -26,7 +30,7 @@ export const ProductUpsertBody = (props:any) => {
             placeholder="Price"
             step=".01"
             // defaultValue={props.product?.Price}
-            value = {props.product.Price}
+            value={props.product.Price}
             onChange={props.handleChange}
           />
           <p className="text-danger"> {props.formErrors.Price}</p>
