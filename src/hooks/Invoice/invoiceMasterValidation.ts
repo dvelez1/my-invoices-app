e44c0dc35ad43c2eef8c112f307f0d5d1d45f258
Dennis Velez <36821370+dvelez1@users.noreply.github.com>
2022-08-19 21:04:27 -0400

    Invoice Master Validation

---
+++ src/hooks/Invoice/invoiceMasterValidation.ts
@@ -0,0 +1,19 @@
+import { InvoiceMaster } from "../../interfaces/InvoiceMaster";
+
+export const addInvoiceDetailsValidation = (values: InvoiceMaster): {} => {
+    const errors: any = {};
+    if (!values.StartDate) {
+      errors.StartDate = "Invoice Date is Required!";
+    }
+
+    if (!values.CustomerId) {
+      errors.CustomerId = "Customer Name is Required!";
+    }
+
+    if (!values.PayedAmount) {
+      errors.PayedAmount = "Payed Amount is Required!";
+    }
+    return errors;
+  };
+
+
