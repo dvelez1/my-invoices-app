95147083c0c2fdd5b4e835212f6299aec4d37a99
Dennis Velez <36821370+dvelez1@users.noreply.github.com>
2022-08-21 19:00:28 -0400

    Fix InvoiceMaster Validation

--- src/hooks/Invoice/invoiceMasterValidation.ts
+++ src/hooks/Invoice/invoiceMasterValidation.ts
@@ -1,19 +1,18 @@
 import { InvoiceMaster } from "../../interfaces/InvoiceMaster";
 
 export const invoiceMasterValidation = (values: InvoiceMaster): {} => {
-    const errors: any = {};
-    if (!values.StartDate) {
-      errors.StartDate = "Invoice Date is Required!";
-    }
+  const errors: any = {};
+  if (!values.StartDate) {
+    errors.StartDate = "Invoice Date is Required!";
+  }
 
-    if (!values.CustomerId) {
-      errors.CustomerId = "Customer Name is Required!";
-    }
+  if (!values.CustomerId) {
+    errors.CustomerId = "Customer Name is Required!";
+  }
 
-    if (!values.PayedAmount) {
+  if (!values.PayedAmount) {
+    if (values.PayedAmount < 0)
       errors.PayedAmount = "Payed Amount is Required!";
-    }
-    return errors;
-  };
-
-
+  }
+  return errors;
+};
