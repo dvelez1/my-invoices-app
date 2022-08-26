4d72a57a9d9c2a6e1fea7679d7b24a23ecb38702
Dennis Velez <36821370+dvelez1@users.noreply.github.com>
2022-08-26 16:48:26 -0400

    Add Invoice Validation

--- src/hooks/Invoice/InvoiceDetailsArrayValidation.ts
+++ src/hooks/Invoice/InvoiceDetailsArrayValidation.ts
@@ -3,21 +3,31 @@ import { InvoiceDetails } from "../../interfaces/InvoiceDetails";
 export const invoiceDetailsValidationSuccess = (
   values: InvoiceDetails[]
 ): [] => {
-
-  var errors:any = [];
+  var errors: any = [];
   if (values.length === 0) return errors.push("Invoice Details not provided!");
 
   for (const invoiceDetails of values) {
     if (!invoiceDetails.ProductId) {
-      return errors.push("Invoice Details: Product not provided!")
+      return errors.push("Invoice Details: Product not provided!");
     }
 
-    if (!invoiceDetails.Price || invoiceDetails.Price <= 0) {
-      return errors.push("Invoice Details: Price not provided or is less than or equal to 0!")
+    if (!invoiceDetails.Price) {
+      if (!Number.isFinite(invoiceDetails.Price))
+        return errors.push(
+          "Invoice Details: Price not provided or is less than 0!"
+        );
+    } else {
+      if (invoiceDetails.Price < 0) {
+        return errors.push(
+          "Invoice Details: Price not provided or is less than 0!"
+        );
+      }
     }
 
     if (!invoiceDetails.Quantity || invoiceDetails.Quantity <= 0) {
-      return errors.push("Invoice Details: Quantity not provided or is less than or equal to 0!")
+      return errors.push(
+        "Invoice Details: Quantity not provided or is less than or equal to 0!"
+      );
     }
   }
 
--- src/hooks/Invoice/addInvoiceDetailsValidation.ts
+++ src/hooks/Invoice/addInvoiceDetailsValidation.ts
@@ -1,29 +1,25 @@
 import { InvoiceDetails } from "../../interfaces/InvoiceDetails";
 
 export const addInvoiceDetailsValidation = (values: InvoiceDetails): {} => {
-    const errors: any = {};
-    if (!values.ProductId) {
-      errors.ProductId = "Product is Required!";
-    }
+  const errors: any = {};
+  if (!values.ProductId) {
+    errors.ProductId = "Product is Required!";
+  }
 
-    if (!values.Price) {
-      errors.Price = "Price is Required!";
+  if (!values.Price) {
+    if (!Number.isFinite(values.Price)) errors.Price = "Price is Required!";
+  } else {
+    if (values.Price < 0) {
+      errors.Price = "Price is less or equal to 0!";
     }
-    else{
-      if (values.Price <=0){
-        errors.Price = "Price is less or equal to 0!";
-      }
-    }
+  }
 
-    if (!values.Quantity) {
-      errors.Quantity = "Quantity is Required!";
+  if (!values.Quantity) {
+    errors.Quantity = "Quantity is Required!";
+  } else {
+    if (values.Quantity <= 0) {
+      errors.Quantity = "Quantity is less or equal to 0!";
     }
-    else{
-      if (values.Quantity <=0){
-        errors.Quantity = "Quantity is less or equal to 0!";
-      }
-    }
-    return errors;
-  };
-
-
+  }
+  return errors;
+};
